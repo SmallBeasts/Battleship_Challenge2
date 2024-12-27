f204d0f5428fe46ccb504603951baf18e4151989
Daniel Spivack <dspivack@hotmail.com>
2024-12-26 23:14:16 -0800

    A bit closer to adding in the Board and starting to parse

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use std::io;
+use std::io::{self, BufRead};
 use std::io::Write;
 use std::ptr::null;
 use std::ptr::null_mut;
@@ -31,18 +31,44 @@ fn create_board(filename: String) -> Board {
     }
 }
 
-fn parse_input(mybuf: String) {
-    let tokens = mybuf.split("--");     // Split on token command
+fn parse_command(mybuf: &str, &mut myboard: Board) {
+    let tokens = mybuf.split_whitespace();
+    let mut state = 0;
     for tok in tokens {
-        println!("{:?}");
+        if tok == "load" {
+            state = 1;
+            continue;
+        }
+        if tok == "guess" {
+            state = 2;
+            continue;
+        }
+        if state == 1 {     // Loading a file so the next token is the filename
+            println!("Loading the file <{}>", tok);
+            continue;
+        }
+        if state == 2 {     // Guess so load up query
+            
+        }
     }
-
+}
+fn eval_input(mybuf: String) {
+    if mybuf.contains("--") {
+        let tokens = mybuf.split("--").filter(|&x| !x.is_empty());     // Split on token command
+        for tok in tokens {
+            println!("What up?! {:?}", tok);
+        }
+    } else {
+        println!("my buf is: {:?}", mybuf);
+    }
+    
 }
 
-fn main() {
+fn main(){
     output_string("Welcome to the Battleship Test Program\nYou can type --help to get a list of commands");
     let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer)?;
-    parse_input(buffer);
+    io::stdin().read_line(&mut buffer);
+    let myboard = create_board(filename);
+    eval_input(buffer);
 
 }
