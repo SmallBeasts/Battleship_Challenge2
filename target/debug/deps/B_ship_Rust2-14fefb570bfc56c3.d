d6a79433369166a2faadfba2a28a28fcc04f4014
Daniel Spivack <dspivack@hotmail.com>
2025-03-30 21:48:01 -0700

    Started AI

--- target/debug/deps/B_ship_Rust2-14fefb570bfc56c3.d
+++ target/debug/deps/B_ship_Rust2-14fefb570bfc56c3.d
@@ -1,6 +1,6 @@
-E:\Danny Rust\Chal2_Bat_rust\target\debug\deps\libB_ship_Rust2-14fefb570bfc56c3.rmeta: src/main.rs src\code\mod.rs src\code\board.rs src\code\commandline.rs src\code\datacheck.rs src\code\enums.rs src\code\file.rs src\code\handler.rs src\code\interactive.rs src\code\ship.rs src\code\utils.rs
+E:\Danny Rust\Chal2_Bat_rust\target\debug\deps\libB_ship_Rust2-14fefb570bfc56c3.rmeta: src/main.rs src\code\mod.rs src\code\board.rs src\code\commandline.rs src\code\datacheck.rs src\code\enums.rs src\code\file.rs src\code\handler.rs src\code\interactive.rs src\code\ship.rs src\code\utils.rs src\code\aistuff.rs
 
-E:\Danny Rust\Chal2_Bat_rust\target\debug\deps\B_ship_Rust2-14fefb570bfc56c3.d: src/main.rs src\code\mod.rs src\code\board.rs src\code\commandline.rs src\code\datacheck.rs src\code\enums.rs src\code\file.rs src\code\handler.rs src\code\interactive.rs src\code\ship.rs src\code\utils.rs
+E:\Danny Rust\Chal2_Bat_rust\target\debug\deps\B_ship_Rust2-14fefb570bfc56c3.d: src/main.rs src\code\mod.rs src\code\board.rs src\code\commandline.rs src\code\datacheck.rs src\code\enums.rs src\code\file.rs src\code\handler.rs src\code\interactive.rs src\code\ship.rs src\code\utils.rs src\code\aistuff.rs
 
 src/main.rs:
 src\code\mod.rs:
@@ -13,3 +13,4 @@ src\code\handler.rs:
 src\code\interactive.rs:
 src\code\ship.rs:
 src\code\utils.rs:
+src\code\aistuff.rs:
--- target/debug/deps/B_ship_Rust2-cd4c8e77b43e767f.d
+++ target/debug/deps/B_ship_Rust2-cd4c8e77b43e767f.d
@@ -1,6 +1,6 @@
-E:\Danny Rust\Chal2_Bat_rust\target\debug\deps\libB_ship_Rust2-cd4c8e77b43e767f.rmeta: src/main.rs src\code\mod.rs src\code\board.rs src\code\commandline.rs src\code\datacheck.rs src\code\enums.rs src\code\file.rs src\code\handler.rs src\code\interactive.rs src\code\ship.rs src\code\utils.rs
+E:\Danny Rust\Chal2_Bat_rust\target\debug\deps\libB_ship_Rust2-cd4c8e77b43e767f.rmeta: src/main.rs src\code\mod.rs src\code\board.rs src\code\commandline.rs src\code\datacheck.rs src\code\enums.rs src\code\file.rs src\code\handler.rs src\code\interactive.rs src\code\ship.rs src\code\utils.rs src\code\aistuff.rs
 
-E:\Danny Rust\Chal2_Bat_rust\target\debug\deps\B_ship_Rust2-cd4c8e77b43e767f.d: src/main.rs src\code\mod.rs src\code\board.rs src\code\commandline.rs src\code\datacheck.rs src\code\enums.rs src\code\file.rs src\code\handler.rs src\code\interactive.rs src\code\ship.rs src\code\utils.rs
+E:\Danny Rust\Chal2_Bat_rust\target\debug\deps\B_ship_Rust2-cd4c8e77b43e767f.d: src/main.rs src\code\mod.rs src\code\board.rs src\code\commandline.rs src\code\datacheck.rs src\code\enums.rs src\code\file.rs src\code\handler.rs src\code\interactive.rs src\code\ship.rs src\code\utils.rs src\code\aistuff.rs
 
 src/main.rs:
 src\code\mod.rs:
@@ -13,3 +13,4 @@ src\code\handler.rs:
 src\code\interactive.rs:
 src\code\ship.rs:
 src\code\utils.rs:
+src\code\aistuff.rs:
